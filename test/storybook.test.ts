813f2770fd0948ec1a741fff664e75eac913d775
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-07-06 11:14:03 +0200

    chore: remove snapshot-related files

--- test/storybook.test.ts
+++
@@ -1,7 +0,0 @@
-import * as nextRouter from 'next/router'
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-nextRouter.useRouter = jest.fn().mockReturnValue({
-  query: {},
-})
