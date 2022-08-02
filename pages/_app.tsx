c5e67e5bf26726e34c5a435ad1cd6b88d4d05f36
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-08-02 16:37:53 +0200

    chore: fix types of App
    Via https://stackoverflow.com/a/67464299

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { mapRawQueryToState } from '@lib/utils/queryUtil'
 import { ParsedUrlQuery } from 'querystring'
 import { StrictMode, FC, ReactElement, ReactNode } from 'react'
 import { Head } from '@components/Head'
@@ -6,24 +5,22 @@ import '../src/style/global.css'
 import { useMatomo } from '@lib/hooks/useMatomo'
 import { MainMenu } from '@components/MainMenu'
 import { NextPage } from 'next'
-import { AppProps } from 'next/app'
+import type { AppProps as NextAppProps } from 'next/app'
+
+type AppProps<P = unknown> = {
+  pageProps: P
+} & Omit<NextAppProps<P>, 'pageProps'>
 
 export interface PagePropType extends Record<string, unknown> {
   title?: string
   query: ParsedUrlQuery
 }
 
-interface ComponentPropType {
-  title?: string
-  query?: ReturnType<typeof mapRawQueryToState>
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement, pageProps: PagePropType) => ReactNode
 }
 
-export type NextPageWithLayout = NextPage<ComponentPropType> & {
-  getLayout?: (page: ReactElement, pageProps: ComponentPropType) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  pageProps: PagePropType
+type AppPropsWithLayout = AppProps<PagePropType> & {
   Component: NextPageWithLayout
 }
 
@@ -37,7 +34,7 @@ const App: FC<AppPropsWithLayout> = ({
 
   return (
     <StrictMode>
-      <Head />
+      <Head pageTitle={pageProps.title || ''} />
       <div className="fixed inset-0 bottom-16 overflow-x-hidden overflow-y-auto">
         {getLayout(<Component {...pageProps} />, pageProps)}
       </div>
