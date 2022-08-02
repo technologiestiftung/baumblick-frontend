dd8fef0d8c36535136ee9ae9949d7bd98e2d63b5
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-08-02 13:17:15 +0200

    feat: fetch tree base data in /trees/[id] route

--- pages/trees/[id].tsx
+++ pages/trees/[id].tsx
@@ -2,28 +2,49 @@ import type { ReactElement, ReactNode } from 'react'
 import { MapLayout } from '@layouts/MapLayout'
 import classNames from 'classnames'
 import { GetServerSideProps, NextPage } from 'next'
+import { getTreeData, TreeDataType } from '@lib/requests/getTreeData'
 
-type TreePageWithLayout = NextPage<{ id: string }> & {
+type TreePageWithLayout = NextPage<{
+  treeData: TreeDataType[]
+}> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  return {
-    props: {
-      id: params?.id,
-    },
+  try {
+    const treeId = typeof params?.id === 'string' ? params.id : null
+
+    if (!treeId || Array.isArray(treeId)) return { notFound: true }
+
+    const treeData = await getTreeData(treeId)
+
+    if (!treeData || treeData.length !== 1)
+      throw new Error('No tree found for this request')
+
+    return {
+      props: {
+        treeData: treeData[0],
+      },
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      notFound: true,
+    }
   }
 }
 
-const TreePage: TreePageWithLayout = ({ id }) => {
+const TreePage: TreePageWithLayout = ({ treeData }) => {
+  console.log(treeData)
+
   return (
     <div
       className={classNames(
         'absolute bottom-0 left-0 min-h-[75vh] bg-white w-full rounded-t-xl shadow-md p-6 z-10'
       )}
     >
-      <h1 className="font-bold">Tree ID {id}</h1>
+      <h1 className="font-bold">Tree ID</h1>
       <h2>Subtitle</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. At magni quo
