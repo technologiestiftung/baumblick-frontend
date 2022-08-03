0cc4d80c4df5972d1c3ca3bf5913bbc533c312a0
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-08-03 12:21:18 +0200

    refactor: darken tree dot stroke color with chroma.js
    Unrelated: the layer ID and keys of the vector tileset were updated to the correct values.

--- src/components/TreesMap/treesLayer.ts
+++ src/components/TreesMap/treesLayer.ts
@@ -1,7 +1,8 @@
 import { LayerSpecification, SourceSpecification } from 'maplibre-gl'
 import colors from '../../style/colors'
+import chroma from 'chroma-js'
 
-export const TREES_LAYER_ID = 'trees'
+export const TREES_LAYER_ID = 'outfull'
 
 export const TREES_SOURCE: SourceSpecification = {
   type: 'vector',
@@ -14,7 +15,7 @@ export const TREES_LAYER: LayerSpecification = {
   id: TREES_LAYER_ID,
   type: 'circle',
   source: TREES_LAYER_ID,
-  'source-layer': 'trees',
+  'source-layer': TREES_LAYER_ID,
   maxzoom: 24,
   minzoom: 0,
   paint: {
@@ -26,7 +27,7 @@ export const TREES_LAYER: LayerSpecification = {
       In reality we will want to interpolate the color based on the Saugspannung value that will be available in the vector tile.
       At that point, the pflanzjahr has to be replaced with the new field's name and the domain changed from years to the values domain.
       */
-      ['get', 'pflanzjahr'],
+      ['get', 'trees_pflanzjahr'],
       0, //0,
       colors.scale['1'],
       1960, //0.125,
@@ -53,23 +54,23 @@ export const TREES_LAYER: LayerSpecification = {
       In reality we will want to interpolate the color based on the Saugspannung value that will be available in the vector tile.
       At that point, the pflanzjahr has to be replaced with the new field's name and the domain changed from years to the values domain.
       */
-      ['get', 'pflanzjahr'],
+      ['get', 'trees_pflanzjahr'],
       0, //0,
-      colors.scale['1-dark'],
+      chroma(colors.scale['1']).darken(0.5).hex(),
       1960, //0.125,
-      colors.scale['2-dark'],
+      chroma(colors.scale['2']).darken(0.5).hex(),
       1970, //0.25,
-      colors.scale['3-dark'],
+      chroma(colors.scale['3']).darken(0.5).hex(),
       1980, //0.375,
-      colors.scale['4-dark'],
+      chroma(colors.scale['4']).darken(0.5).hex(),
       1990, //0.5,
-      colors.scale['5-dark'],
+      chroma(colors.scale['5']).darken(0.5).hex(),
       2000, //0.625,
-      colors.scale['6-dark'],
+      chroma(colors.scale['6']).darken(0.5).hex(),
       2010, //0.75,
-      colors.scale['7-dark'],
+      chroma(colors.scale['7']).darken(0.5).hex(),
       2020, //0.875,
-      colors.scale['8-dark'],
+      chroma(colors.scale['8']).darken(0.5).hex(),
     ],
     'circle-radius': [
       'interpolate',
