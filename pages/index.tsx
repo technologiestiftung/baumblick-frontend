423edc049287630171bff12dca06ca5847f616cf
Lucas Vogel <contact@vogelino.com>
2022-08-03 13:02:31 +0200

    feat(Home): Add CTA and legal links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@components/Header'
 import { GetServerSideProps } from 'next'
 import { FC } from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import Trans from 'next-translate/Trans'
+import { HomeSlider } from '@components/HomeSlider'
+import { Button } from '@components/Button'
+import { GPS } from '@components/Icons'
+import useTranslation from 'next-translate/useTranslation'
+import { LegalLinks } from '@components/LegalLinks'
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getServerSideProps: GetServerSideProps = async ({ query }) => ({
@@ -13,70 +15,19 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => ({
   },
 })
 
-const HomeSlide: FC = ({ children }) => (
-  <div className="px-4">
-    <div className="border border-gray-200 rounded-md h-96 grid grid-rows-[1fr,auto] grid-cols-1">
-      <div></div>
-      <p className="p-4 font-serif border-t border-gray-100">{children}</p>
-    </div>
-  </div>
-)
-
-const LegendDot: FC<{ className: string }> = ({ className }) => (
-  <span
-    className={`${className} border w-3 h-3 inline-block rounded-full align-middle`}
-  />
-)
-
 export const Home: FC = () => {
-  const formattingComponents = [
-    /* eslint-disable react/jsx-key */
-    <strong />,
-    <LegendDot className="bg-scale-8 border-scale-8-dark" />,
-    <LegendDot className="bg-scale-1 border-scale-1-dark" />,
-    /* eslint-enable react/jsx-key */
-  ]
+  const { t } = useTranslation('common')
   return (
     <>
       <Header />
-      <Slider arrows={false} infinite={false} dots>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.1"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.2"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.3"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.4"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.5"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-        <HomeSlide>
-          <Trans
-            i18nKey="common:home.slides.6"
-            components={formattingComponents}
-          />
-        </HomeSlide>
-      </Slider>
+      <HomeSlider />
+      <div className="px-4 py-8">
+        <Button href="/map" primary className="w-full">
+          <GPS />
+          {t('home.cta')}
+        </Button>
+        <LegalLinks />
+      </div>
     </>
   )
 }
---
+++ src/components/HomeSlider/index.tsx
@@ -0,0 +1,69 @@
+import Trans from 'next-translate/Trans'
+import { FC } from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+
+const HomeSlide: FC = ({ children }) => (
+  <div className="px-4">
+    <div className="border border-gray-200 rounded-md h-96 grid grid-rows-[1fr,auto] grid-cols-1">
+      <div></div>
+      <p className="p-4 font-serif border-t border-gray-100">{children}</p>
+    </div>
+  </div>
+)
+
+const LegendDot: FC<{ className: string }> = ({ className }) => (
+  <span
+    className={`${className} border w-3 h-3 inline-block rounded-full align-middle`}
+  />
+)
+
+export const HomeSlider: FC = () => {
+  const formattingComponents = [
+    /* eslint-disable react/jsx-key */
+    <strong />,
+    <LegendDot className="bg-scale-8 border-scale-8-dark" />,
+    <LegendDot className="bg-scale-1 border-scale-1-dark" />,
+    /* eslint-enable react/jsx-key */
+  ]
+  return (
+    <Slider arrows={false} infinite={false} dots>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.1"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.2"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.3"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.4"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.5"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+      <HomeSlide>
+        <Trans
+          i18nKey="common:home.slides.6"
+          components={formattingComponents}
+        />
+      </HomeSlide>
+    </Slider>
+  )
+}
