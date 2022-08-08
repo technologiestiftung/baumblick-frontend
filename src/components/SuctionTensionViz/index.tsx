083eb84a8c79969ca74ed7bbaa33a86a0821c2b3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-08-08 15:46:25 +0200

    refactor: name viz more appropriately

---
+++ src/components/SuctionTensionViz/index.tsx
@@ -0,0 +1,54 @@
+import { RootsIllustration } from '@components/RootsIllustration'
+import { SuctionTensionLevel } from '@lib/utils/mapSuctionTensionToLevel'
+import classNames from 'classnames'
+import { FC } from 'react'
+import colors from 'src/style/colors'
+import { AverageCircle } from './AverageCircle'
+import { SoilLayer } from './SoilLayer'
+
+export interface SuctionTensionVizType {
+  depth30Level: SuctionTensionLevel
+  depth60Level: SuctionTensionLevel
+  depth90Level: SuctionTensionLevel
+  averageLevel: SuctionTensionLevel
+}
+
+export const SuctionTensionViz: FC<SuctionTensionVizType> = ({
+  depth30Level,
+  depth60Level,
+  depth90Level,
+  averageLevel,
+}) => {
+  return (
+    <div className="w-full relative overflow-hidden">
+      <SoilLayer depth={30} level={depth30Level} />
+      <SoilLayer depth={60} level={depth60Level} />
+      <SoilLayer depth={90} level={depth90Level} />
+      <div
+        className={classNames(
+          'absolute top-0',
+          'w-full h-full',
+          'flex justify-center'
+        )}
+      >
+        <RootsIllustration
+          color={colors.gray[200]}
+          additionalClasses={classNames(
+            'flex-shrink-0',
+            'w-auto h-5/6',
+            'mix-blend-multiply'
+          )}
+        />
+      </div>
+      <div
+        className={classNames(
+          'absolute top-0 right-0',
+          'w-2/3 md:w-1/2 h-full',
+          'flex justify-center items-center'
+        )}
+      >
+        <AverageCircle level={averageLevel} />
+      </div>
+    </div>
+  )
+}
