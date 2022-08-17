9b6c37adf346bee798a2dd9c2f7ce1cfc6859eed
Lucas Vogel <contact@vogelino.com>
2022-08-17 11:11:21 +0200

    feat(QTREE-369): Add confirmation to FeedbackReportForm

--- src/components/FeedbackReportForm/FeedbackReportForm.tsx
+++ src/components/FeedbackReportForm/FeedbackReportForm.tsx
@@ -1,20 +1,24 @@
 import { Button } from '@components/Button'
+import { Check } from '@components/Icons'
 import { useImageLoadsSuccessfully } from '@lib/hooks/useLoadImage'
 import classNames from 'classnames'
 import useTranslation from 'next-translate/useTranslation'
 import { FC } from 'react'
+import colors from '../../style/colors'
 
 export interface FeedbackReportFormPropType {
   title: string
   description: string
   imageUrl?: string
   onButtonClick?: () => void
+  alreadyReported?: boolean
 }
 
 export const FeedbackReportForm: FC<FeedbackReportFormPropType> = ({
   title,
   description,
   imageUrl,
+  alreadyReported = false,
   onButtonClick = () => undefined,
 }) => {
   const { t } = useTranslation('common')
@@ -24,7 +28,7 @@ export const FeedbackReportForm: FC<FeedbackReportFormPropType> = ({
   return (
     <div
       className={classNames(
-        'grid p-6 gap-x-4 gap-y-2 border-b border-gray-200',
+        'grid p-6 gap-x-6 gap-y-2 border-b border-gray-200',
         showImg && 'pr-0 grid-cols-[8fr,5fr]'
       )}
     >
@@ -37,13 +41,32 @@ export const FeedbackReportForm: FC<FeedbackReportFormPropType> = ({
         />
       )}
       <p className="font-serif">{description}</p>
-      <Button
-        onClick={onButtonClick}
-        className={classNames(showImg && 'col-span-2 mr-6 ', 'mt-4')}
-        primary
-      >
-        {t('feedback.reportButton', { title })}
-      </Button>
+      {alreadyReported && (
+        <div
+          className={classNames(
+            'grid grid-cols-[1fr,24px] gap-x-6',
+            showImg && 'col-span-2 mr-6',
+            'mt-4'
+          )}
+        >
+          <h4 className="font-bold">{t('feedback.confirmationTitle')}</h4>
+          <div className="row-span-2 flex items-center">
+            <Check color1={colors.scale['1']} />
+          </div>
+          <p className="font-serif text-gray-500">
+            {t('feedback.confirmationDescription')}
+          </p>
+        </div>
+      )}
+      {!alreadyReported && (
+        <Button
+          onClick={onButtonClick}
+          className={classNames(showImg && 'col-span-2 mr-6 ', 'mt-4')}
+          primary
+        >
+          {t('feedback.reportButton', { title })}
+        </Button>
+      )}
     </div>
   )
 }
