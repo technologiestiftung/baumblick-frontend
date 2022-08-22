c436e35f78cb93c9e155683bf2860bf2fc6fb236
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-08-22 09:09:45 +0200

    test: update tests to new scale

--- src/components/SuctionTensionViz/SuctionTensionViz.test.tsx
+++ src/components/SuctionTensionViz/SuctionTensionViz.test.tsx
@@ -5,46 +5,62 @@ describe('SuctionTensionViz', () => {
   test('renders all levels and average circle', () => {
     render(
       <SuctionTensionViz
-        depth30Level={2}
-        depth60Level={1}
-        depth90Level={3}
-        averageLevel={3}
+        depth30LevelId={'critical'}
+        depth60LevelId={'good'}
+        depth90LevelId={'medium'}
+        averageLevelId={'medium'}
       />
     )
 
-    const level30El = screen.getByLabelText(`Tiefe: 30 cm, Stufe: 2`)
+    const level30El = screen.getByLabelText(
+      `Tiefe: 30 cm, Wasserversorgung: Kritisch`
+    )
     expect(level30El).toBeInTheDocument()
 
-    const level60El = screen.getByLabelText(`Tiefe: 60 cm, Stufe: 1`)
+    const level60El = screen.getByLabelText(
+      `Tiefe: 60 cm, Wasserversorgung: Gut`
+    )
     expect(level60El).toBeInTheDocument()
 
-    const level90El = screen.getByLabelText(`Tiefe: 90 cm, Stufe: 3`)
+    const level90El = screen.getByLabelText(
+      `Tiefe: 90 cm, Wasserversorgung: Mäßig`
+    )
     expect(level90El).toBeInTheDocument()
 
-    const averageEl = screen.getByLabelText(`Durchschnitt-Stufe: 3`)
+    const averageEl = screen.getByLabelText(
+      `Durschnittliche Wasserversorgung: Mäßig`
+    )
     expect(averageEl).toBeInTheDocument()
   })
 
   test('falls back to gray colors without provided values', () => {
     render(
       <SuctionTensionViz
-        depth30Level={2}
-        depth60Level={undefined}
-        depth90Level={undefined}
-        averageLevel={undefined}
+        depth30LevelId={'critical'}
+        depth60LevelId={undefined}
+        depth90LevelId={undefined}
+        averageLevelId={undefined}
       />
     )
 
-    const level30El = screen.getByLabelText(`Tiefe: 30 cm, Stufe: 2`)
+    const level30El = screen.getByLabelText(
+      `Tiefe: 30 cm, Wasserversorgung: Kritisch`
+    )
     expect(level30El).toBeInTheDocument()
 
-    const level60El = screen.getByLabelText(`Tiefe: 60 cm, Stufe: unbekannt`)
+    const level60El = screen.getByLabelText(
+      `Tiefe: 60 cm, Wasserversorgung: unbekannt`
+    )
     expect(level60El).toBeInTheDocument()
 
-    const level90El = screen.getByLabelText(`Tiefe: 90 cm, Stufe: unbekannt`)
+    const level90El = screen.getByLabelText(
+      `Tiefe: 90 cm, Wasserversorgung: unbekannt`
+    )
     expect(level90El).toBeInTheDocument()
 
-    const averageEl = screen.getByLabelText(`Durchschnitt-Stufe: unbekannt`)
+    const averageEl = screen.getByLabelText(
+      `Durschnittliche Wasserversorgung: unbekannt`
+    )
     expect(averageEl).toBeInTheDocument()
   })
 })
--- src/lib/utils/mapSuctionTensionToLevel/mapSuctionTensionToLevel.test.ts
+++ src/lib/utils/mapSuctionTensionToLevel/mapSuctionTensionToLevel.test.ts
@@ -2,24 +2,16 @@ import { mapSuctionTensionToLevel } from '.'
 
 describe('mapSuctionTensionToLevel', () => {
   test('is first level with matching suction tension', () => {
-    const level = mapSuctionTensionToLevel(25)
-    expect(level).toBe(1)
+    const level = mapSuctionTensionToLevel(15)
+    expect(level?.id).toBe('good')
   })
   test('is second level with matching suction tension', () => {
-    const level = mapSuctionTensionToLevel(75)
-    expect(level).toBe(2)
+    const level = mapSuctionTensionToLevel(40)
+    expect(level?.id).toBe('medium')
   })
   test('is third level with matching suction tension', () => {
     const level = mapSuctionTensionToLevel(125)
-    expect(level).toBe(3)
-  })
-  test('is fourth level with matching suction tension', () => {
-    const level = mapSuctionTensionToLevel(175)
-    expect(level).toBe(4)
-  })
-  test('is fifth level with matching suction tension', () => {
-    const level = mapSuctionTensionToLevel(225)
-    expect(level).toBe(5)
+    expect(level?.id).toBe('critical')
   })
   test('is undefined with negative suction tension', () => {
     const level = mapSuctionTensionToLevel(-1)
