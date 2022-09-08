0a78c1e00fc2cece047078c4338ca122db55a55c
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-09-08 16:59:03 +0200

    fix: vercel env

--- src/lib/utils/urlUtil/index.ts
+++ src/lib/utils/urlUtil/index.ts
@@ -7,7 +7,9 @@ export const treeUrlSlugToId = (urlSlug: string): string =>
   urlSlug.replaceAll(DOT_REPLACE_CHAR, '.')
 
 export const getBaseUrl = (): string => {
-  const isProd = process.env.NODE_ENV === 'production'
+  const isProd =
+    process.env.NODE_ENV === 'production' ||
+    process.env.VERCEL_ENV === 'production'
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
   return isProd
     ? baseUrl
