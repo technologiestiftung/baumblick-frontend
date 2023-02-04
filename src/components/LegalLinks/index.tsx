97daf058cc93fec60183a56936309ae41ae4ba97
DESKTOP-AA9IQHH\steff <steffen.haensch@web.de>
2023-02-04 21:43:36 +0100

    refactor: changed leftover BEM names.

--- src/components/LegalLinks/index.tsx
+++ src/components/LegalLinks/index.tsx
@@ -11,21 +11,27 @@ export const LegalLinks: FC = () => {
         'screen1200:justify-start screen1200:border-t screen1200:border-solid screen1200:border-uiLightGrey'
       )}
     >
-      <a
-        className={classNames(
-          'focus:outline-none focus:ring-offset-4 focus:ring-offset-white focus:ring-2 focus:ring-gray-900 hover:text-gray-900 transition-colors',
-          'font-sans text-uiMidGrey not-italic font-semibold text-base leading-6 w-40 flex justify-center items-center'
-        )}
-        href="https://www.technologiestiftung-berlin.de/de/impressum/"
-      >
-        <span>{t('legal.imprint')}</span>
-      </a>
-      <a
-        className="footer__link focus:outline-none focus:ring-offset-4 focus:ring-offset-white focus:ring-2 focus:ring-gray-900 hover:text-gray-900 transition-colors"
-        href="https://www.technologiestiftung-berlin.de/de/datenschutz/"
-      >
-        <span>{t('legal.privacy')}</span>
-      </a>
+      {[
+        {
+          title: t('legal.imprint'),
+          href: 'https://www.technologiestiftung-berlin.de/de/impressum/',
+        },
+        {
+          title: t('legal.privacy'),
+          href: 'https://www.technologiestiftung-berlin.de/de/datenschutz/',
+        },
+      ].map((link, linkIndex) => (
+        <a
+          key={linkIndex}
+          className={classNames(
+            'focus:outline-none focus:ring-offset-4 focus:ring-offset-white focus:ring-2 focus:ring-gray-900 hover:text-gray-900 transition-colors',
+            'font-sans text-uiMidGrey not-italic font-semibold text-base leading-6 w-40 flex justify-center items-center'
+          )}
+          href={link.href}
+        >
+          <span>{link.title}</span>
+        </a>
+      ))}
     </section>
   )
 }
