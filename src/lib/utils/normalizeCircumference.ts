d2531e501110a58c453ed0ae12143f10b2a3d858
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-08 09:51:27 +0100

    feat: use correct value for trunk circumference util

--- src/lib/utils/normalizeCircumference.ts
+++ src/lib/utils/normalizeCircumference.ts
@@ -1,12 +1,14 @@
 import { mapToRange } from './mapToRange'
 
-const MIN_CIRCUMFERENCE = 0
-const MAX_CIRCUMFERENCE = 250
+const MIN_CIRCUMFERENCE = 0 // in mm
+const MAX_CIRCUMFERENCE = 800 // in mm
 
 /**
  * Function that normalizes a tree circumference to a value between 0 and 1.
  * @param inputCircumference: number
  * @returns number
  */
-export const normalizeCircumference = (inputCircumference: number): number =>
+export const normalizeTrunkCircumference = (
+  inputCircumference: number
+): number =>
   mapToRange(inputCircumference, [MIN_CIRCUMFERENCE, MAX_CIRCUMFERENCE], [0, 1])
