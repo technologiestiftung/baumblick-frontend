7347e4e74da3604508885440f8be3e91b35abc1e
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-08 12:31:45 +0100

    feat(home): use real into image

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,5 +1,4 @@
 import useTranslation from 'next-translate/useTranslation'
-import classNames from 'classnames'
 import { FC } from 'react'
 import { Button } from '@components/Button'
 import { ArrowDown } from '@components/Icons'
@@ -11,15 +10,10 @@ export const Intro: FC = () => {
     <div className="bg-green py-[5.625rem] screen1200:py-[20vh]">
       <ContentGrid>
         <GridDefault>
-          <div
-            className={classNames(
-              'relative',
-              'after:block after:absolute after:top-0 after:w-full after:h-full',
-              'after:bg-scale-good after:mix-blend-hue'
-            )}
-          >
-            <img src="/images/intro-image.jpg" alt="" />
-          </div>
+          <img
+            src="/images/berlin_trees.webp"
+            alt="Straßenbäume in Berlin rund um eine U-Bahn-Station"
+          />
           <div className="mt-6 mb-12 screen1200:mt-12">
             {['home.intro.text.1', 'home.intro.text.2'].map((text, index) => {
               return (
