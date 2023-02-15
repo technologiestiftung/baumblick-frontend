4fa09f3c107416a420de57945de3a6309a06bcd3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-15 14:40:19 +0100

    refactor: reuse abstract ImageCard
    DRY's up some parts.

--- src/components/FeedbackRequestsList/index.tsx
+++ src/components/FeedbackRequestsList/index.tsx
@@ -1,9 +1,11 @@
-import { FeedbackReportForm } from '@components/FeedbackReportForm'
+import { Button } from '@components/Button'
 import { FeedbackReportModal } from '@components/FeedbackReportModal'
+import { ImageCard } from '@components/ImageCard'
 import { IssueTypeType, useFeedbackData } from '@lib/hooks/useFeedbackData'
 import { TreeDataType } from '@lib/requests/getTreeData'
 import useTranslation from 'next-translate/useTranslation'
 import { FC, useState } from 'react'
+import { FeedbackConfirmation } from './FeedbackConfirmation'
 
 interface FeedbackRequestsListPropType {
   treeData: TreeDataType | undefined
@@ -27,11 +29,20 @@ export const FeedbackRequestsList: FC<FeedbackRequestsListPropType> = ({
     <div className="relative bg-white z-0">
       {!error &&
         issues?.map((issueType) => (
-          <FeedbackReportForm
+          <ImageCard
             key={issueType.id}
-            {...issueType}
-            onButtonClick={() => setOpenedIssueModal(issueType)}
-          />
+            title={issueType.title}
+            description={issueType.description}
+            imageUrl={issueType.imageUrl || ''}
+          >
+            {issueType.alreadyReported ? (
+              <FeedbackConfirmation />
+            ) : (
+              <Button primary onClick={() => setOpenedIssueModal(issueType)}>
+                {t('feedback.reportButton', { title: issueType.title })}
+              </Button>
+            )}
+          </ImageCard>
         ))}
       <FeedbackReportModal
         title={openedIssueModal?.title || ' '}
