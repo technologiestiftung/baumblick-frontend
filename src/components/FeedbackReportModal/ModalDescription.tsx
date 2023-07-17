44341a8b061a030aa8555a2f3847dd32fda4f80a
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-07-17 15:25:58 +0200

    feat: inform users about purpose of messages

--- src/components/FeedbackReportModal/ModalDescription.tsx
+++ src/components/FeedbackReportModal/ModalDescription.tsx
@@ -2,6 +2,7 @@ import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { FC } from 'react'
 import { FeedbackReportModalPropType } from './FeedbackReportModal'
+import classNames from 'classnames'
 
 export const ModalDescription: FC<
   Pick<FeedbackReportModalPropType, 'title' | 'address' | 'treeName'>
@@ -13,16 +14,49 @@ export const ModalDescription: FC<
   }
 
   return (
-    <Trans
-      i18nKey="common:feedback.modalHelp"
-      components={formattingComponents}
-      values={{
-        address: address
-          ? `${t('feedback.proximityWord')} <bold>${address}</bold>`
-          : '',
-        title,
-        treeName,
-      }}
-    />
+    <>
+      <div>
+        <Trans
+          i18nKey="common:feedback.modalHelp"
+          components={formattingComponents}
+          values={{
+            address: address
+              ? `${t('feedback.proximityWord')} <bold>${address}</bold>`
+              : '',
+            title,
+            treeName,
+          }}
+        />
+      </div>
+      <div
+        className={classNames(
+          'mt-3',
+          'text-xs font-medium font-sans',
+          'border p-2'
+        )}
+      >
+        <Trans
+          i18nKey="common:feedback.modalDisclaimer"
+          components={[
+            // Anchor does have content, provided by our locale file.
+            // eslint-disable-next-line jsx-a11y/anchor-has-content
+            <a
+              key="link"
+              className={classNames(
+                'px-0.5',
+                'underline',
+                'transition-colors focus:outline-none',
+                'focus:ring-2 focus:ring-gray-600',
+                'hover:text-gray-500'
+              )}
+              href="https://ordnungsamt.berlin.de/frontend/meldungNeu/wo"
+              aria-label={t('common:feedback.modalDisclaimerAriaLabel')}
+              target="_blank"
+              rel="noopener noreferrer"
+            />,
+          ]}
+        />
+      </div>
+    </>
   )
 }
